300b191792213e0975f570b689c990d54d8ab472
Axel De Acetis <axel@deacetis.cz>
2023-08-30 23:43:07 +0200

    chore: change svgtofont imports to ES Modules

--- scripts/svg2font.js
+++ scripts/svg2font.js
@@ -1,5 +1,9 @@
-const svgtofont = require('svgtofont')
-const fs = require('fs')
+import svgtofont from 'svgtofont'
+import fs from 'fs'
+import path from 'path'
+
+const __filename = new URL(import.meta.url).pathname;
+const __dirname = path.dirname(__filename);
 
 const HEADER = `/* AUTO VAR INJECTION HEADER !! DO NOT MOVE !! */`
 const FOOTER = `/* AUTO VAR INJECTION FOOTER !! DO NOT MOVE !! */`
