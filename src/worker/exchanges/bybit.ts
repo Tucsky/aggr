7e623695d35df82ac63a25bee5c73cfb3a9a27c9
Tucsky <tucsky@pm.me>
2023-11-16 20:23:02 +0100

    bump bybit api to v5

--- src/worker/exchanges/bybit.ts
+++ src/worker/exchanges/bybit.ts
@@ -1,48 +1,64 @@
 import Exchange from '../exchange'
 
+const SPOT_PAIR_REGEX = /-SPOT$/
+const TRADE_TOPIC_REGEX = /^publicTrade\./
+const SPOT_WS = 'wss://stream.bybit.com/v5/public/spot'
+const LINEAR_WS = 'wss://stream.bybit.com/v5/public/linear'
+const INVERSE_WS = 'wss://stream.bybit.com/v5/public/inverse'
+
 export default class BYBIT extends Exchange {
   id = 'BYBIT'
+
+  private types: { [pair: string]: 'spot' | 'linear' | 'inverse' }
+
   protected endpoints = {
     PRODUCTS: [
-      'https://api.bybit.com/spot/v1/symbols',
-      'https://api.bybit.com/v2/public/symbols'
+      'https://api.bybit.com/v5/market/instruments-info?category=spot', // BTCUSDT -> BTCUSDT-SPOT
+      'https://api.bybit.com/v5/market/instruments-info?category=linear', // BTCUSDT, BTCPERP, BTC-03NOV23
+      'https://api.bybit.com/v5/market/instruments-info?category=inverse' // BTCUSD, BTCUSDH24
     ]
   }
 
   async getUrl(pair) {
-    if (/-SPOT$/.test(pair)) {
-      return 'wss://stream.bybit.com/spot/quote/ws/v2'
+    if (this.types[pair] === 'spot') {
+      return SPOT_WS
     }
 
-    return pair.indexOf('USDT') !== -1
-      ? 'wss://stream.bybit.com/realtime_public'
-      : 'wss://stream.bybit.com/realtime'
+    if (this.types[pair] === 'linear') {
+      return LINEAR_WS
+    }
+
+    return INVERSE_WS
+  }
+
+  validateProducts(data) {
+    if (!data.types) {
+      return false
+    }
+
+    return true
   }
 
   formatProducts(response) {
     const products = []
+    const types = {}
 
     for (const data of response) {
-      const type = ['spot', 'futures'][response.indexOf(data)]
+      const type = ['spot', 'linear', 'inverse'][response.indexOf(data)]
 
-      for (const product of data.result) {
-        if (type === 'spot') {
-          products.push(product.name + '-SPOT')
-        } else {
-          if (product.status !== 'Trading') {
-            continue
-          }
+      for (const product of data.result.list) {
+        const symbol = `${product.symbol}${type === 'spot' ? '-SPOT' : ''}`
 
-          products.push(product.name)
-        }
+        products.push(symbol)
+        types[symbol] = type
       }
     }
 
     return {
-      products
+      products,
+      types
     }
   }
-
   /**
    * Sub
    * @param {WebSocket} api
@@ -53,28 +69,26 @@ export default class BYBIT extends Exchange {
       return
     }
 
-    if (/-SPOT$/.test(pair)) {
-      api.send(
-        JSON.stringify({
-          topic: 'trade',
-          event: 'sub',
-          params: { binary: false, symbol: pair.replace(/-SPOT$/, '') }
-        })
-      )
-    } else {
-      api.send(
-        JSON.stringify({
-          op: 'subscribe',
-          args: ['trade.' + pair, 'liquidation.' + pair]
-        })
-      )
+    const isSpot = this.types[pair] === 'spot'
+    const realPair = isSpot ? pair.replace(SPOT_PAIR_REGEX, '') : pair
+    const topics = [`publicTrade.${realPair}`]
+
+    if (!isSpot) {
+      topics.push(`liquidation.${realPair}`)
     }
+
+    api.send(
+      JSON.stringify({
+        op: 'subscribe',
+        args: topics
+      })
+    )
 
     return true
   }
 
   /**
-   * Sub
+   * Unsub
    * @param {WebSocket} api
    * @param {string} pair
    */
@@ -83,24 +97,60 @@ export default class BYBIT extends Exchange {
       return
     }
 
-    if (/-SPOT$/.test(pair)) {
-      api.send(
-        JSON.stringify({
-          topic: 'trade',
-          event: 'cancel',
-          params: { binary: false, symbol: pair.replace(/-SPOT$/, '') }
-        })
-      )
-    } else {
-      api.send(
-        JSON.stringify({
-          op: 'unsubscribe',
-          args: ['trade.' + pair, 'liquidation.' + pair]
-        })
-      )
+    const isSpot = this.types[pair] === 'spot'
+    const realPair = isSpot ? pair.replace(SPOT_PAIR_REGEX, '') : pair
+    const topics = [`publicTrade.${realPair}`]
+
+    if (!isSpot) {
+      topics.push(`liquidation.${realPair}`)
     }
 
+    api.send(
+      JSON.stringify({
+        op: 'unsubscribe',
+        args: topics
+      })
+    )
+
     return true
+  }
+
+  formatTrade(trade, isSpot) {
+    let size = +trade.v
+    let pair = trade.s
+
+    if (!isSpot && this.types[trade.s] === 'inverse') {
+      size /= trade.p
+    } else if (isSpot) {
+      pair += '-SPOT'
+    }
+
+    return {
+      exchange: this.id,
+      pair,
+      timestamp: +trade.T,
+      price: +trade.p,
+      size,
+      side: trade.S === 'Buy' ? 'buy' : 'sell'
+    }
+  }
+
+  formatLiquidation(liquidation) {
+    let size = +liquidation.size
+
+    if (this.types[liquidation.symbol] === 'inverse') {
+      size /= liquidation.price
+    }
+
+    return {
+      exchange: this.id,
+      pair: liquidation.symbol,
+      timestamp: +liquidation.updatedTime,
+      size,
+      price: +liquidation.price,
+      side: liquidation.side === 'Buy' ? 'sell' : 'buy',
+      liquidation: true
+    }
   }
 
   onMessage(event, api) {
@@ -110,56 +160,25 @@ export default class BYBIT extends Exchange {
       return
     }
 
-    if (json.data.length) {
-      return this.emitTrades(
-        api.id,
-        json.data.map(trade => {
-          const size = /USDT$/.test(trade.symbol)
-            ? trade.size
-            : trade.size / trade.price
+    if (json.data) {
+      if (TRADE_TOPIC_REGEX.test(json.topic)) {
+        const isSpot = api.url === SPOT_WS
 
-          return {
-            exchange: this.id,
-            pair: trade.symbol,
-            timestamp: +new Date(trade.timestamp),
-            price: +trade.price,
-            size: size,
-            side: trade.side === 'Buy' ? 'buy' : 'sell'
-          }
-        })
-      )
-    } else if (!json.data.symbol) {
-      return this.emitTrades(api.id, [
-        {
-          exchange: this.id,
-          pair: json.params.symbol + '-SPOT',
-          timestamp: +json.data.t,
-          price: +json.data.p,
-          size: +json.data.q,
-          side: json.data.m ? 'buy' : 'sell'
-        }
-      ])
-    } else {
-      const size = /USDT$/.test(json.data.symbol)
-        ? +json.data.qty
-        : json.data.qty / json.data.price
-
-      return this.emitLiquidations(api.id, [
-        {
-          exchange: this.id,
-          pair: json.data.symbol,
-          timestamp: +json.data.time,
-          price: +json.data.price,
-          size: size,
-          side: json.data.side === 'Buy' ? 'sell' : 'buy',
-          liquidation: true
-        }
-      ])
+        return this.emitTrades(
+          api.id,
+          json.data.map(trade => this.formatTrade(trade, isSpot))
+        )
+      } else {
+        return this.emitLiquidations(
+          api.id,
+          [this.formatLiquidation(json.data)]
+        )
+      }
     }
   }
 
   onApiCreated(api) {
-    this.startKeepAlive(api, { op: 'ping' }, 45000)
+    this.startKeepAlive(api, { op: 'ping' }, 20000)
   }
 
   onApiRemoved(api) {
