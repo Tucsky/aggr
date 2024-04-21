fd2cf0a2ccef6bcc92131d7d5a0769dc098d1ae6
Axel De Acetis <20586054+adeacetis@users.noreply.github.com>
2024-04-21 17:17:22 +0200

    fix: file path in dist-server/index.mjs

--- dist-server/index.mjs
+++ dist-server/index.mjs
@@ -5,13 +5,20 @@ import { config } from 'dotenv'
 config()
 
 const __filename = new URL(import.meta.url).pathname
-const __dirname = path.dirname(__filename)
+let __dirname = path.dirname(__filename)
+
+if(process.platform === "win32"){
+	__dirname = __dirname.substring(1)
+}
 
 const app = express()
 const PORT = process.env.DIST_SERVER_PORT || 8060
 app.use(express.static(path.join(__dirname, '..', 'dist')))
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'dist', 'index.html'))
+  console.log(__filename, __dirname)
+  res.sendFile('index.html', {
+    root: __dirname + '/../dist'
+  })
 })
 app.listen(PORT, () => {
   console.log(`aggr dist server started on ${PORT}`)
