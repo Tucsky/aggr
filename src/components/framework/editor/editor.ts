2c80f7162e4f61765a9bf27ca5d7b95f8e8894f1
Tucsky <tucsky@pm.me>
2024-10-16 22:22:24 +0200

    feat: autocomplete local variable in script

--- src/components/framework/editor/editor.ts
+++ src/components/framework/editor/editor.ts
@@ -2,13 +2,8 @@ import 'monaco-editor/esm/vs/editor/editor.all.js'
 import 'monaco-editor/esm/vs/language/typescript/monaco.contribution'
 import 'monaco-editor/esm/vs/basic-languages/monaco.contribution'
 
-import {
-  editor,
-  languages,
-  Range
-} from 'monaco-editor/esm/vs/editor/editor.api'
-import AGGR_SUGGESTIONS from './suggestions'
-import { loadMd, showReference, TOKENS } from './references'
+import { editor, languages } from 'monaco-editor/esm/vs/editor/editor.api'
+import { provideCompletionItems, provideHover } from './completion'
 
 languages.typescript.javascriptDefaults.setCompilerOptions({
   noLib: true,
@@ -22,86 +17,11 @@ languages.typescript.javascriptDefaults.setDiagnosticsOptions({
 })
 
 languages.registerCompletionItemProvider('javascript', {
-  provideCompletionItems: function (model, position) {
-    const word = model.getWordUntilPosition(position)
-    const range = {
-      startLineNumber: position.lineNumber,
-      endLineNumber: position.lineNumber,
-      startColumn: word.startColumn,
-      endColumn: word.endColumn
-    }
-
-    const queryFilter = new RegExp(`${word}`, 'i')
-
-    return {
-      suggestions: AGGR_SUGGESTIONS.filter(
-        a => queryFilter.test(a.label) || queryFilter.test(a.detail)
-      ).map(s => ({
-        ...s,
-        kind: languages.CompletionItemKind.Function,
-        range
-      }))
-    }
-  }
+  provideCompletionItems
 })
 
 languages.registerHoverProvider('javascript', {
-  provideHover: async function (model, position) {
-    // Get the word at the current position
-    const word = model.getWordAtPosition(position)
-
-    if (!word || !word.word) {
-      return
-    }
-
-    const token = word.word.replace(/^plot/, '')
-
-    // Check if the word is one of the specific tokens
-    if (TOKENS.indexOf(token) !== -1) {
-      const md = await loadMd(token)
-      let contents
-      if (md) {
-        contents = md
-          .split(/\n\n/)
-          .slice(0, 2)
-          .map(row => ({
-            value: row
-          }))
-          .concat({
-            value: `[Learn more](${token})`
-          })
-
-        setTimeout(() => {
-          const linkElement = document.querySelector(`a[data-href="${token}"]`)
-
-          if (!linkElement) {
-            return
-          }
-
-          linkElement.addEventListener('click', (event: MouseEvent) => {
-            event.preventDefault()
-
-            showReference(token, md, {
-              x: event.clientX,
-              y: event.clientY
-            })
-          })
-        }, 500)
-      } else {
-        contents = [{ value: 'no definition found' }]
-      }
-
-      return {
-        range: new Range(
-          position.lineNumber,
-          word.startColumn,
-          position.lineNumber,
-          word.endColumn
-        ),
-        contents
-      }
-    }
-  }
+  provideHover
 })
 
 export default editor
