6c4f18807d38f7821970a5bab72a3778f5cef144
Tucsky <tucsky@pm.me>
2024-10-29 17:28:24 +0100

    feat: markdown editor for indicators description

--- src/components/framework/editor/editor.ts
+++ src/components/framework/editor/editor.ts
@@ -2,8 +2,10 @@ import 'monaco-editor/esm/vs/editor/editor.all.js'
 import 'monaco-editor/esm/vs/language/typescript/monaco.contribution'
 import 'monaco-editor/esm/vs/basic-languages/monaco.contribution'
 
+import store from '@/store'
 import { editor, languages } from 'monaco-editor/esm/vs/editor/editor.api'
 import { provideCompletionItems, provideHover } from './completion'
+import { rgbToHex, splitColorCode } from '@/utils/colors'
 
 languages.typescript.javascriptDefaults.setCompilerOptions({
   noLib: true,
@@ -23,5 +25,42 @@ languages.registerCompletionItemProvider('javascript', {
 languages.registerHoverProvider('javascript', {
   provideHover
 })
+
+const lsLight = store.state.settings.theme === 'light'
+const style = getComputedStyle(document.documentElement)
+const backgroundColor = splitColorCode(
+  style.getPropertyValue('--theme-background-base')
+)
+const backgroundColor100 = splitColorCode(
+  style.getPropertyValue('--theme-background-100')
+)
+const backgroundColor150 = splitColorCode(
+  style.getPropertyValue('--theme-background-150')
+)
+
+if (lsLight) {
+  editor.defineTheme('aggr', {
+    base: 'vs',
+    inherit: true,
+    rules: [],
+    colors: {
+      'minimap.background': rgbToHex(backgroundColor150),
+      'editor.background': rgbToHex(backgroundColor100),
+      'editor.lineHighlightBackground': rgbToHex(backgroundColor),
+      'editor.lineHighlightBorder': rgbToHex(backgroundColor)
+    }
+  })
+} else {
+  editor.defineTheme('aggr', {
+    base: 'vs-dark',
+    inherit: true,
+    rules: [],
+    colors: {
+      'editor.background': rgbToHex(backgroundColor100),
+      'editor.lineHighlightBackground': rgbToHex(backgroundColor),
+      'editor.lineHighlightBorder': rgbToHex(backgroundColor)
+    }
+  })
+}
 
 export default editor
