4ccfae257fdd69d2b3021d7e06cd9782747259dd
Tucsky <tucsky@pm.me>
2025-01-02 21:01:53 +0100

    gh-pages

---
+++ time.5c4cb14.js
@@ -0,0 +1,1 @@
+const e="```ts\ntime: number\n```\n\nIndicates the Unix timestamp of the current bar, expressed in seconds. This value represents the specific point in time associated with the current bar in the chart.";export{e as default};
