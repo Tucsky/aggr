7473dba52b8bc2c005b01425a1e535c28294ac51
Tucsky <tucsky@pm.me>
2025-11-28 15:58:24 +0100

    gh-pages

---
+++ cbuy.5c4cb14.js
@@ -0,0 +1,1 @@
+const t="```ts\ncbuy: number\n```\n\nRepresents the sum of all the individuals buy / long market orders in the current bar.\nIt's the count, not the volume.\n> Disabling a market in the chart filter UI will affect this amount\n\n\n```ts\nhistogram(cbuy, title='buy count')\n```";export{t as default};
